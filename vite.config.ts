2d8b40d6ef285892f7537b38157b0f277b74b787
medvony <medvony@gmail.com>
2026-01-30 01:21:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    
-    build: {
-      outDir: 'dist'
+  plugins: [react()],
+  
+  optimizeDeps: {
+    exclude: ['tesseract.js']
+  },
+  
+  build: {
+    outDir: 'dist',
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
     }
-  };
+  }
 });
